1a0fb6524d9ead7da67bf1dff4adfd540bdb76b0
Kyle <kyleyee522@gmail.com>
2024-03-21 21:47:35 -0700

    added get and post methods

---
+++ routes/notes.js
@@ -0,0 +1,32 @@
+const notes = require('express').Router();
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+notes.get('/', (req, res) => {
+	readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+	const { title, text } = req.body;
+
+	if (title && text) {
+		const newNote = {
+			title,
+			text,
+			id: uuidv4(),
+		};
+
+		readAndAppend(newNote, './db/db.json');
+
+		const response = {
+			status: 'success',
+			body: newNote,
+		};
+
+		res.json(response);
+	} else {
+		res.json('Error in posting note');
+	}
+});
+
+module.exports = notes;
